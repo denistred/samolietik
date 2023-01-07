3c78b933d7aa05e3326c95da96d384689299342b
denistred <denislylov2004@mail.ru>
2023-01-07 14:37:00 +0300

    good player control

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 import os
 import sys
@@ -17,37 +19,75 @@ def load_image(name, colorkey=None):
 
 class Player(pygame.sprite.Sprite):
     image = load_image(PLAYER_SPRITE)
+    MAX_SPEED = 10
 
-    def __init__(self, velocity, bullet_count):
+    def __init__(self, acceleration, bullet_count):
         super().__init__()
         self.image = Player.image
         self.rect = self.image.get_rect()
         self.rect.bottom = WINDOW_SIZE[1] - 50
         self.rect.centerx = WINDOW_SIZE[0] // 2
-        self.velocity = velocity
+
+        self.acceleration = acceleration
         self.bullet_count = bullet_count
+        self.velocity = [0, 0]
 
-    def move_right(self):
+    def check_max_speed(self):
+        if self.velocity[0] > Player.MAX_SPEED:
+            self.velocity[0] = Player.MAX_SPEED
+        elif self.velocity[0] < -Player.MAX_SPEED:
+            self.velocity[0] = -Player.MAX_SPEED
+
+    def accelerate_right(self):
+        self.velocity[0] += self.acceleration[0]
+        self.check_max_speed()
+
+
+    def accelerate_left(self):
+        self.velocity[0] -= self.acceleration[0]
+        self.check_max_speed()
+
+    def auto_acceleration_stop(self):
+        if self.velocity[0] > 0:
+            if self.velocity[0] - math.sqrt(abs(self.velocity[0])) > 0:
+                self.velocity[0] -= math.sqrt(abs(self.velocity[0]))
+            else:
+                self.velocity[0] -= self.velocity[0]
+
+        elif self.velocity[0] < 0:
+            if self.velocity[0] + math.sqrt(abs(self.velocity[0])) < 0:
+                self.velocity[0] += math.sqrt(abs(self.velocity[0]))
+            else:
+                self.velocity[0] -= self.velocity[0]
+
+    def check_position(self):
+        if self.rect.x > WINDOW_SIZE[0] - self.rect.width:
+            self.rect.x = WINDOW_SIZE[0] - self.rect.width
+        elif self.rect.x < 0:
+            self.rect.x = 0
+    def move(self):
+        print(self.velocity[0])
         self.rect.right += self.velocity[0]
-
-    def move_left(self):
-        self.rect.right -= self.velocity[0]
+        self.check_position()
+        self.auto_acceleration_stop()
 
 
 class Game:
     def __init__(self, screen):
         self.all_sprites = pygame.sprite.Group()
         self.screen = screen
+        self.player_key = None
 
-    def keys_handler(self, key):
-        if key[pygame.K_d]:
-            self.player.move_right()
-        if key[pygame.K_a]:
-            self.player.move_left()
-
+    def keys_handler(self, key, ev_type):
+        if key[pygame.K_d] and ev_type == pygame.KEYDOWN:
+            self.player_key = 0
+        if key[pygame.K_a] and ev_type == pygame.KEYDOWN:
+            self.player_key = 1
+        if ev_type == pygame.KEYUP and not key[pygame.K_a] and not key[pygame.K_d]:
+            self.player_key = None
 
     def start_game(self):
-        self.player = Player((20, 20), 50)
+        self.player = Player((5, 5), 50)
         self.all_sprites.add(self.player)
 
     def render(self):
@@ -55,6 +95,13 @@ class Game:
         self.all_sprites.update()
         self.all_sprites.draw(self.screen)
 
+    def player_physic(self):
+        print(self.player_key)
+        if self.player_key == 0:
+            self.player.accelerate_right()
+        elif self.player_key == 1:
+            self.player.accelerate_left()
+        self.player.move()
 
 
 if __name__ == '__main__':
@@ -72,8 +119,10 @@ if __name__ == '__main__':
                 running = False
 
             key = pygame.key.get_pressed()
-            game.keys_handler(key)
+            ev_type = event.type
+            game.keys_handler(key, ev_type)
 
+        game.player_physic()
         game.render()
         pygame.display.flip()
         clock.tick(60)
